4be8c30a02b694321f637fbf861eee3eccc18d32
Alessandro <37877991+aledefra@users.noreply.github.com>
2023-04-23 18:17:28 +0200

    Update contract address

--- src/config.mainnet.ts
+++ src/config.mainnet.ts
@@ -52,7 +52,7 @@ export const network: NetworkType = {
   gatewayAddress: 'https://gateway.multiversx.com',
   explorerAddress: 'https://explorer.multiversx.com',
   delegationContract:
-    'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqx8llllsxavffq'
+    'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqp90llllsz3fftm'
 };
 
 export const delegationContractData: DelegationContractType[] = [
